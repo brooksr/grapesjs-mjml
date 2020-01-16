88b6bbc68dc9b7e4f3e86cd986f85629f65dcecb
brooksr <brooks@brooksroche.com>
2020-01-16 14:32:55 -0800

    move blocks to array

--- src/blocks.js
+++ src/blocks.js
@@ -8,113 +8,123 @@ export default (editor, opt = {}) => {
   const preheader = document.getElementById("preheader").value;
 
   opt.resetBlocks && bm.getAll().reset();
-  
-  bm.add('mj-wrapper', {
+
+  let blocks = [{
+    name: 'mj-wrapper',
     label: 'Wrapper',
     content: `<mj-wrapper></mj-wrapper>`,
-    attributes: { class: 'fa fa-object-group' },
-    ...allBlocks,
-  });
-
-  bm.add('mj-section', {
+    attributes: {
+      class: 'fa fa-object-group'
+    }
+  }, {
+    name: 'mj-section',
     label: 'Section',
     content: `<mj-section>
-        <mj-column></mj-column>
-      </mj-section>`,
-    attributes: { class: 'gjs-fonts gjs-f-b1' },
-    ...allBlocks,
-  });
-
-  bm.add('mj-column', {
+          <mj-column></mj-column>
+        </mj-section>`,
+    attributes: {
+      class: 'gjs-fonts gjs-f-b1'
+    }
+  }, {
+    name: 'mj-column',
     label: 'Column',
     content: `<mj-column></mj-column>`,
-    attributes: { class: 'gjs-fonts gjs-f-b2' },
-    ...allBlocks,
-  });
-
-  bm.add('mj-text', {
+    attributes: {
+      class: 'gjs-fonts gjs-f-b2'
+    }
+  }, {
+    name: 'mj-text',
     label: 'Text',
     content: `<mj-text>Hello,<br />Thank you for visiting.<br /><br />Sincerely,<br /><b>${from}</b></mj-text>`,
-    attributes: { class: 'gjs-fonts gjs-f-text' },
-    ...allBlocks,
-  });
-
-  bm.add('mj-navbar', {
+    attributes: {
+      class: 'gjs-fonts gjs-f-text'
+    }
+  }, {
+    name: 'mj-navbar',
     label: 'NavBar',
     content: `<mj-navbar>
-    <mj-navbar-link>Getting started</mj-navbar-link>
-    <mj-navbar-link>Try it live</mj-navbar-link>
-    <mj-navbar-link>Templates</mj-navbar-link>
-    <mj-navbar-link>Components</mj-navbar-link>
-    </mj-navbar>`,
-    attributes: { class: 'fa fa-bars' },
-    ...allBlocks,
-  });
-
-  bm.add('mj-navbar-link', {
+      <mj-navbar-link>Getting started</mj-navbar-link>
+      <mj-navbar-link>Try it live</mj-navbar-link>
+      <mj-navbar-link>Templates</mj-navbar-link>
+      <mj-navbar-link>Components</mj-navbar-link>
+      </mj-navbar>`,
+    attributes: {
+      class: 'fa fa-bars'
+    }
+  }, {
+    name: 'mj-navbar-link',
     label: 'NavBar Link',
     content: `<mj-navbar-link>Link 1</mj-navbar-link>`,
-    attributes: { class: 'gjs-fonts gjs-f-button' },
-    ...allBlocks,
-  });
-
-  bm.add('mj-hero', {
+    attributes: {
+      class: 'gjs-fonts gjs-f-button'
+    }
+  }, {
+    name: 'mj-hero',
     label: 'Hero Element',
     content: `<mj-hero mode="fixed-height" height="469px" background-width="600px" background-height="469px" background-url="https://via.placeholder.com/600x300?text=BACKGROUND" background-color="#2a3448" padding="100px 0px">
-      <mj-text padding="20px" color="#ffffff" align="center" font-size="45px" line-height="45px" font-weight="900">
-        GO TO SPACE
-      </mj-text>
-      <mj-button href="https://mjml.io/" align="center">
-        ORDER YOUR TICKET NOW
-      </mj-button>
-    </mj-hero>`,
-    attributes: { class: 'fa fa-square' },
-    ...allBlocks,
-  });
-
-  bm.add('mj-button', {
+        <mj-text padding="20px" color="#ffffff" align="center" font-size="45px" line-height="45px" font-weight="900">
+          GO TO SPACE
+        </mj-text>
+        <mj-button href="https://mjml.io/" align="center">
+          ORDER YOUR TICKET NOW
+        </mj-button>
+      </mj-hero>`,
+    attributes: {
+      class: 'fa fa-square'
+    }
+  }, {
+    name: 'mj-button',
     label: 'Button',
     content: '<mj-button>Button</mj-button>',
-    attributes: { class: 'gjs-fonts gjs-f-button' },
-    ...allBlocks,
-  });
-
-  bm.add('mj-image', {
+    attributes: {
+      class: 'gjs-fonts gjs-f-button'
+    }
+  }, {
+    name: 'mj-image',
     label: 'Image',
     content: '<mj-image src="https://via.placeholder.com/650x250?text=MJ-IMAGE">',
-    attributes: { class: 'fa fa-image' },
-    ...allBlocks,
-  });
-
-  bm.add('mj-divider', {
+    attributes: {
+      class: 'fa fa-image'
+    }
+  }, {
+    name: 'mj-divider',
     label: 'Divider',
     content: '<mj-divider/>',
-    attributes: { class: 'gjs-fonts gjs-f-divider' },
-    ...allBlocks,
-  });
-
-  bm.add('mj-spacer', {
+    attributes: {
+      class: 'gjs-fonts gjs-f-divider'
+    }
+  }, {
+    name: 'mj-spacer',
     label: 'Spacer',
     content: '<mj-spacer/>',
-    attributes: { class: 'fa fa-arrows-v' },
-    ...allBlocks,
-  });
-
-  bm.add('mj-social-group', {
-    label: 'Group Social',
+    attributes: {
+      class: 'fa fa-arrows-v'
+    }
+  }, {
+    name: 'mj-social-element',
+    label: 'Social Group',
     content: `<mj-social font-size="12px" icon-size="24px" border-radius="12px" mode="horizontal">
-        <mj-social-element name="facebook"></mj-social-element>
-        <mj-social-element name="google"></mj-social-element>
-        <mj-social-element name="twitter"></mj-social-element>
-      </mj-social>`,
-    attributes: { class: 'fa fa-share-alt' },
-    ...allBlocks,
+          <mj-social-element name="facebook-noshare"></mj-social-element>
+          <mj-social-element name="google-noshare"></mj-social-element>
+          <mj-social-element name="twitter-noshare"></mj-social-element>
+        </mj-social>`,
+    attributes: {
+      class: 'fa fa-share-alt'
+    }
+  }, {
+    name: 'mj-social',
+    label: 'Social Element',
+    content: '<mj-social-element name="facebook-noshare" />',
+    attributes: {
+      class: 'fa fa-share-alt'
+    }
+  }];
+
+  blocks.forEach(function (block) {
+    bm.add(block.name, {
+      ...block,
+      ...allBlocks
+    });
   });
 
-  bm.add('mj-social-element', {
-    label: 'Social Element',
-    content: '<mj-social-element name="facebook" />',
-    attributes: { class: 'fa fa-share-alt' },
-    ...allBlocks,
-  });
 };
